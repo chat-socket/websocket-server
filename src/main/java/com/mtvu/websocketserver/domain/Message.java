f561d41aba88a75bb39fc68b2095d08addd9fda9
VU Manh Tu <glmanhtu@gmail.com>
2022-12-30 16:34:51 +0700

    Optimise code and update dependencies

--- src/main/java/com/mtvu/websocketserver/domain/Message.java
+++ src/main/java/com/mtvu/websocketserver/domain/Message.java
@@ -1,45 +1,19 @@
 package com.mtvu.websocketserver.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 /**
  * @author mvu
  * @project chat-socket
  **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message implements Serializable {
-
-    private String message;
     private String groupId;
-
-    public Message() {
-    }
-
-    public Message(String groupId, String message) {
-        this.message = message;
-        this.groupId = groupId;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
-
-    @Override
-    public String toString() {
-        return "Message{" +
-            "message='" + message + '\'' +
-            ", groupId='" + groupId + '\'' +
-            '}';
-    }
+    private String message;
 }
--- src/main/java/com/mtvu/websocketserver/domain/TransferMessage.java
+++ src/main/java/com/mtvu/websocketserver/domain/TransferMessage.java
@@ -1,55 +1,27 @@
 package com.mtvu.websocketserver.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
-import java.time.Instant;
+import java.time.OffsetDateTime;
 
 /**
  * @author mvu
  * @project chat-socket
  **/
+@Data
+@NoArgsConstructor
 public class TransferMessage implements Serializable {
-    private final String from;
-    private final String groupId;
-    private final long timestamp;
-    private final String message;
-
-    public TransferMessage(String from, String groupId, long timestamp, String message) {
-        this.from = from;
-        this.groupId = groupId;
-        this.timestamp = timestamp;
-        this.message = message;
-    }
+    private String from;
+    private String groupId;
+    private OffsetDateTime timestamp;
+    private String message;
 
     public TransferMessage(Message message, String from) {
         this.from = from;
         this.groupId = message.getGroupId();
         this.message = message.getMessage();
-        this.timestamp = Instant.now().getEpochSecond();
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    @Override
-    public String toString() {
-        return "TransferMessage{" +
-            "from='" + from + '\'' +
-            ", groupId='" + groupId + '\'' +
-            ", timestamp=" + timestamp +
-            ", message='" + message + '\'' +
-            '}';
+        this.timestamp = OffsetDateTime.now();
     }
 }
