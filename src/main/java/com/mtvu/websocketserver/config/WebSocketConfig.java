d02f9ad5c295d3e759ab13fa50d94908fce3f0d9
VU Manh Tu <glmanhtu@gmail.com>
2023-01-07 03:36:40 +0700

    Set same origin policy to all, for testing

--- src/main/java/com/mtvu/websocketserver/config/WebSocketConfig.java
+++ src/main/java/com/mtvu/websocketserver/config/WebSocketConfig.java
@@ -60,8 +60,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket");
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/websocket")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
