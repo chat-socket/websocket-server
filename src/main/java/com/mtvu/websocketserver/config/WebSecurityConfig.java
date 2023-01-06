f7bcba7fc4173376190a1d9a97c215ee28801aef
VU Manh Tu <glmanhtu@gmail.com>
2023-01-07 03:23:08 +0700

    Exposing /actuator/** for kubernetes to check pod alive

--- src/main/java/com/mtvu/websocketserver/config/WebSecurityConfig.java
+++ src/main/java/com/mtvu/websocketserver/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.mtvu.websocketserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,14 +23,14 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors().and()
             .csrf().disable()
             .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeRequests(authorize -> authorize
-                .requestMatchers("/websocket", "/websocket/**").permitAll()
+                .requestMatchers("/actuator/**", "/websocket", "/websocket/**").permitAll()
                 .anyRequest().denyAll());
         return http.build();
     }
-
 
     @Bean
     public AuthenticationProvider getJwsAuthenticationProvider(JwtDecoder decoder) {
