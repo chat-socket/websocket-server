ac62bdc0bb7bcfd2a93f3d1977bf6bb2e6262bde
VU Manh Tu <glmanhtu@gmail.com>
2023-01-27 12:49:35 +0700

    Init quarkus websocket project

--- src/main/java/com/mtvu/websocketserver/WebsocketServerApplication.java
+++ src/main/java/com/mtvu/websocketserver/WebsocketServerApplication.java
@@ -1,18 +1,56 @@
 package com.mtvu.websocketserver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * @author mvu
- * @project chat-socket
- **/
-@SpringBootApplication
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
+
+
+@ServerEndpoint("/chat/{username}")
+@ApplicationScoped
 public class WebsocketServerApplication {
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebsocketServerApplication.class, args);
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessions.put(username, session);
     }
 
-}
+    @OnClose
+    public void onClose(Session session, @PathParam("username") String username) {
+        sessions.remove(username);
+        broadcast("User " + username + " left");
+    }
 
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+        sessions.remove(username);
+        broadcast("User " + username + " left on error: " + throwable);
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) {
+        if (message.equalsIgnoreCase("_ready_")) {
+            broadcast("User " + username + " joined");
+        } else {
+            broadcast(">> " + username + ": " + message);
+        }
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result ->  {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+    }
+}
